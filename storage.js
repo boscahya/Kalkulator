68ed7308cfc51fe9d4cc203583fd69491279231b
boscahya <63669371+boscahya@users.noreply.github.com>
2020-04-15 10:43:14 +0700

    uploda Program Kalkulator

---
+++ storage.js
@@ -0,0 +1,80 @@
+const CACHE_KEY = "calculation_history";
+ 
+function checkForStorage() {
+   return typeof(Storage) !== "undefined";
+}
+ 
+function putHistory(data) {
+   if (checkForStorage()) {
+       let historyData = null;
+       if (localStorage.getItem(CACHE_KEY) === null) {
+           historyData = [];
+       } else {
+           historyData = JSON.parse(localStorage.getItem(CACHE_KEY));
+       }
+ 
+       historyData.unshift(data);
+ 
+       if (historyData.length > 5) {
+           historyData.pop();
+       }
+ 
+       localStorage.setItem(CACHE_KEY, JSON.stringify(historyData));
+   }
+}
+ 
+function showHistory() {
+   if (checkForStorage) {
+       return JSON.parse(localStorage.getItem(CACHE_KEY)) || [];
+   } else {
+       return [];
+   }
+}
+ 
+function renderHistory() {
+   const historyData = showHistory();
+   let historyList = document.querySelector("#historyList");
+   historyList.innerHTML = "";
+ 
+   for (let history of historyData) {
+       let row = document.createElement('tr');
+       row.innerHTML = "<td>" + history.firstNumber + "</td>";
+       row.innerHTML += "<td>" + history.operator + "</td>";
+       row.innerHTML += "<td>" + history.secondNumber + "</td>";
+       row.innerHTML += "<td>" + history.result + "</td>";
+ 
+       historyList.appendChild(row);
+   }
+}
+ 
+renderHistory();
+// const history = {
+//        firstNumber: calculator.firstNumber,
+//        secondNumber: calculator.displayNumber,
+//        operator: calculator.operator,
+//        result: result
+// // }
+// function performCalculation() {
+//    if (calculator.firstNumber == null || calculator.operator == null) {
+//        alert("Anda belum menetapkan operator");
+//        return;
+//    }
+ 
+//    let result = 0;
+//    if (calculator.operator === "+") {
+//        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+//    } else {
+//        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber)
+//    }
+ 
+//    // objek yang akan dikirimkan sebagai argumen fungsi putHistory()
+//    const history = {
+//        firstNumber: calculator.firstNumber,
+//        secondNumber: calculator.displayNumber,
+//        operator: calculator.operator,
+//        result: result
+//    }
+//    putHistory(history);
+//    calculator.displayNumber = result;
+//    renderHistory();
+// }
